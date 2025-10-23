0f875017df2a58126787385fff53d436b5a0a2c9
iCodeNobell <76748114+nobelleon@users.noreply.github.com>
2025-10-23 22:19:34 +0700

    Add files via upload

---
+++ roundabstractartapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoundabstractartappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'roundabstractartapp'
---
+++ roundabstractartapp/static/js/round_abstract_art.js
@@ -0,0 +1,140 @@
+const sphere = document.getElementById('sphere');
+const resolution = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--sphere-resolution'));
+const radius = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--sphere-radius'));
+for (let lat = 0; lat <= resolution; lat++) {
+    const theta = (lat * Math.PI) / resolution;
+    const sinTheta = Math.sin(theta);
+    const cosTheta = Math.cos(theta);
+    for (let lon = 0; lon <= resolution; lon++) {
+        const phi = (lon * 2 * Math.PI) / resolution;
+        const x = radius * Math.sin(phi) * sinTheta;
+        const y = radius * cosTheta;
+        const z = radius * Math.cos(phi) * sinTheta;
+        const dot = document.createElement('div');
+        dot.style.transform = ` translate3d(${x}px, ${y}px, ${z}px) rotateY(${(-phi * 180) / Math.PI}deg) rotateX(${(-theta * 180) / Math.PI}deg)`;
+        dot.style.animation = ` pulse 6s infinite ease-out`;
+        dot.style.animationDelay = `${theta*1.2}s`;
+        sphere.appendChild(dot);
+    }
+}
+
+document.body.addEventListener('click', ()=>{
+    document.body.classList.toggle('inversion');
+})
+
+
+// ------------------------------------------------------------------- //
+// -----------------------  CUSTOM DOT CURSOR  ----------------------- //
+
+var cursor = {
+    delay: 8,
+    _x: 0,
+    _y: 0,
+    endX: (window.innerWidth / 2),
+    endY: (window.innerHeight / 2),
+    cursorVisible: true,
+    cursorEnlarged: false,
+    $dot: document.querySelector('.cursor-dot'),
+    $outline: document.querySelector('.cursor-dot-outline'),
+    
+    init: function() {
+        // Set up element sizes
+        this.dotSize = this.$dot.offsetWidth;
+        this.outlineSize = this.$outline.offsetWidth;
+        
+        this.setupEventListeners();
+        this.animateDotOutline();
+    },
+    
+    setupEventListeners: function() {
+        var self = this;
+        
+        // Anchor hovering
+        document.querySelectorAll('a').forEach(function(el) {
+            el.addEventListener('mouseover', function() {
+                self.cursorEnlarged = true;
+                self.toggleCursorSize();
+            });
+            el.addEventListener('mouseout', function() {
+                self.cursorEnlarged = false;
+                self.toggleCursorSize();
+            });
+        });
+        
+        // Click events
+        document.addEventListener('mousedown', function() {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+        });
+        document.addEventListener('mouseup', function() {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+        });
+  
+  
+        document.addEventListener('mousemove', function(e) {
+            // Show the cursor
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+
+            // Position the dot
+            self.endX = e.pageX;
+            self.endY = e.pageY;
+            self.$dot.style.top = self.endY + 'px';
+            self.$dot.style.left = self.endX + 'px';
+        });
+        
+        // Hide/show cursor
+        document.addEventListener('mouseenter', function(e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+        });
+        
+        document.addEventListener('mouseleave', function(e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+        });
+    },
+    
+    animateDotOutline: function() {
+        var self = this;
+        
+        self._x += (self.endX - self._x) / self.delay;
+        self._y += (self.endY - self._y) / self.delay;
+        self.$outline.style.top = self._y + 'px';
+        self.$outline.style.left = self._x + 'px';
+        
+        requestAnimationFrame(this.animateDotOutline.bind(self));
+    },
+    
+    toggleCursorSize: function() {
+        var self = this;
+        
+        if (self.cursorEnlarged) {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+        } else {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+        }
+    },
+    
+    toggleCursorVisibility: function() {
+        var self = this;
+        
+        if (self.cursorVisible) {
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+        } else {
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+        }
+    }
+}
+
+cursor.init();
+
